8b53a142a9c77e50f7b815ff21b94a9689e27287
OlegarioLopez <olelopezlinares@gmail.com>
2022-02-27 19:12:12 +0000

    send data functionality cleaned

--- app/src/main/java/com/ole/citastatto/MainActivity.kt
+++ app/src/main/java/com/ole/citastatto/MainActivity.kt
@@ -1,64 +1,40 @@
 package com.ole.citastatto
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.ole.citastatto.data.Month
+import androidx.compose.runtime.*
 import com.ole.citastatto.ui.theme.CitasTattoTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
 
-    private val appointmentCollectionRef = Firebase.firestore.collection("Appointments")
+    private val montViewModel by viewModels<MontViewModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             CitasTattoTheme {
-                ButtonAppointment()
+                Column() {
+                    ButonRetrieveData()
+                    Text(text = montViewModel.name.value?:" fallo al cargar")
+                }
             }
         }
     }
 
-     private fun saveAppointment(appointment: Month) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            appointmentCollectionRef.add(appointment).await()
-            withContext(Dispatchers.Main) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "éxito en el guardado en firestore",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        } catch (e: Exception) {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
 
     @Composable
-    fun ButtonAppointment() {
-        Button(
-            onClick = {
-                val month=Month(31,"Marzo",3,2)
-                saveAppointment(month)
-            },
-        ) {
-            Text(text = "Botón")
-        }
+    fun ButonRetrieveData() {
+            Button(onClick = { montViewModel.retrieveMonths()}){
+                Text(text = "Cargar datos")
+            }
     }
-
 }
 
 
