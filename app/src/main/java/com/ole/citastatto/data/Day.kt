9e97ee3c20be3bfca1fe83c03c74aecaffefd675
OlegarioLopez <olelopezlinares@gmail.com>
2022-03-05 19:27:23 +0000

    Day dataclass Updated

--- app/src/main/java/com/ole/citastatto/data/Day.kt
+++ app/src/main/java/com/ole/citastatto/data/Day.kt
@@ -5,16 +5,17 @@ import java.time.LocalTime
 data class Day(
     val weekDay: String = "",
     val dayInMonth: Int = 0,
-    val startMorning: List<Int> = listOf<Int>(0,0),
-    val finishMorning: List<Int> = listOf<Int>(0,0),
-    val startEvening: List<Int> = listOf<Int>(0,0),
-    val finishtEvening: List<Int> = listOf<Int>(0,0),
+    val startMorning: List<Int> = listOf(0,0),
+    val finishMorning: List<Int> = listOf(0,0),
+    val startEvening: List<Int> = listOf(0,0),
+    val finishtEvening: List<Int> = listOf(0,0),
     var stripes: MutableList<Stripe> = mutableListOf()
 ){
     private var startMorningTime: LocalTime? = LocalTime.of(0,0)
     private var finishMorningTime: LocalTime? = LocalTime.of(0,0)
     private var startEveningTime: LocalTime? = LocalTime.of(0,0)
     private var finishtEveningTime: LocalTime? = LocalTime.of(0,0)
+
    init{
        startMorningTime=LocalTime.of(startMorning[0],startMorning[1])
        finishMorningTime=LocalTime.of(finishMorning[0],finishMorning[1])
