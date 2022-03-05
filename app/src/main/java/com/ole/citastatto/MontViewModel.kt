5a119c071f8572305c55e51670d905be516f481b
OlegarioLopez <olelopezlinares@gmail.com>
2022-03-05 19:29:39 +0000

    month state added to have acces to the whole database in the code

--- app/src/main/java/com/ole/citastatto/MontViewModel.kt
+++ app/src/main/java/com/ole/citastatto/MontViewModel.kt
@@ -9,6 +9,8 @@ import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import com.ole.citastatto.data.Day
 import com.ole.citastatto.data.Month
+import com.ole.citastatto.data.Stripe
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
@@ -16,8 +18,9 @@ import kotlinx.coroutines.withContext
 
 //TODO: Revisar si es mejor hacer el querySnapshot una sola vez para toda la clase y cómo hacerlo
 class MontViewModel : ViewModel() {
-    private val _name = mutableStateOf("SIN CARGA")
-    var name: State<String> = _name
+
+    private val _month = mutableStateOf(Month())
+    var month: State<Month> = _month
 
     private val _daysAvailables = mutableStateOf<MutableList<Day>>(mutableListOf())
     var daysAvailables: State<MutableList<Day>> = _daysAvailables
@@ -27,18 +30,21 @@ class MontViewModel : ViewModel() {
     fun retrieveMonths() {
         viewModelScope.launch(Dispatchers.IO) {
             val querySnapshot = monthCollectionRef.whereEqualTo("monthName", "APRIL").get().await()
-            val sb = StringBuilder()
+            var month: Month? = Month()
             for (document in querySnapshot.documents) {
-                val month = document.toObject<Month>()
+                month = document.toObject<Month>()
 
-                sb.append(month?.monthName)
             }
-            withContext(Dispatchers.Main) { _name.value = sb.toString() }
+            withContext(Dispatchers.Main) {
+                if (month != null) {
+                    _month.value = month
+                }
+            }
 
         }
     }
 
-    fun retrieveStripes(duration: Int) {
+    fun retrieveAvailableStripes(duration: Int) {
 
         viewModelScope.launch(Dispatchers.IO) {
 
@@ -60,12 +66,11 @@ class MontViewModel : ViewModel() {
                                 index++
                             }
 
-                        if (day.stripes.isNotEmpty()) {
+                            if (day.stripes.isNotEmpty()) {
 
+                                auxDaysWithStripe.add(day)
 
-                            auxDaysWithStripe.add(day)
-
-                        }
+                            }
                         }
                     }
                 }
@@ -78,55 +83,36 @@ class MontViewModel : ViewModel() {
     }
 
     // si hay hueco de 3 a 7 y la cita dura una hora sólo se debe ofrecer de 3 a 4 y de 6 a 7 para juntar las citas
-   /* fun splitStripes(daysStripesNoSplited: MutableList<Day>, duration: Int): MutableList<Day> {
-        for (day in daysStripesNoSplited) {
-            var auxStripe = Stripe()
-            for (stripe in day.stripes) {
-                var split = 1
-                if (stripe.duration > duration && split < 2) { // si el hueco libre dura lo mismo que la cita, no se puede splitear
-                    auxStripe.momentIni = stripe.momentIni
-                    for (moment in day.moments) {
-                        var auxStripe2 = Stripe(
-                            momentIni = stripe.momentIni,
-                            momentFin = moment
-                        ) //triquiñuela para calcular el final del primer split
-                        auxStripe2.durationCalc()
-                        if (auxStripe2.duration == duration) {
-                            auxStripe.momentFin = moment
+     /*fun splitStripes(daysStripesNoSplited: MutableList<Day>, duration: Int): MutableList<Day> {
+         for (day in daysStripesNoSplited) {
+             var auxStripe = Stripe()
+             for (stripe in day.stripes) {
+                 var split = 1
+                 if (stripe.duration > duration && split < 2) { // si el hueco libre dura lo mismo que la cita, no se puede splitear
+                     auxStripe.momentIni = stripe.momentIni
+                     for (moment in day.moments) {
+                         var auxStripe2 = Stripe(
+                             momentIni = stripe.momentIni,
+                             momentFin = moment
+                         ) //triquiñuela para calcular el final del primer split
+                         auxStripe2.durationCalc()
+                         if (auxStripe2.duration == duration) {
+                             auxStripe.momentFin = moment
 
-                        }
-                    }
-                }
-            }
-        }
+                         }
+                     }
+                 }
+             }
+         }
 
-        return mutableListOf()
-    }
-    private fun bookAppointment( newStripeMap: Map<String, Any>) = CoroutineScope(Dispatchers.IO).launch {
-        val monthQuery = monthCollectionRef
-            .whereEqualTo("firstName", person.firstName)
-            .whereEqualTo("lastName", person.lastName)
-            .whereEqualTo("age", person.age)
-            .get()
-            .await()
-        if(personQuery.documents.isNotEmpty()) {
-            for(document in personQuery) {
-                try {
-                    //personCollectionRef.document(document.id).update("age", newAge).await()
-                    personCollectionRef.document(document.id).set(
-                        newPersonMap,
-                        SetOptions.merge()
-                    ).await()
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
+         return mutableListOf()
+     }*/
+      /*fun bookAppointment(newStripeMap: Stripe, day: Day, monthNumber: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+
             }
-        } else {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, "No persons matched the query.", Toast.LENGTH_LONG).show()
-            }
-        }
-    }*/
+                //withContext(Dispatchers.Main) {
+            }*/
+
+
 }
