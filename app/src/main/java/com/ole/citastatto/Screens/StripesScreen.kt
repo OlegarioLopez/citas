db58f862c1732438393a747bc8f64b72e8aec374
OlegarioLopez <olelopezlinares@gmail.com>
2022-03-31 11:52:43 +0100

    Update StripesScreen.kt

--- app/src/main/java/com/ole/citastatto/Screens/StripesScreen.kt
+++ app/src/main/java/com/ole/citastatto/Screens/StripesScreen.kt
@@ -1,16 +1,13 @@
 package com.ole.citastatto.Screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,59 +33,67 @@ import java.time.format.FormatStyle
 
         val daysList = monthViewModel.daysAvailables.value
         val spotsList = monthViewModel.spotsAvailables.value
-        androidx.compose.material.Surface(
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-        ) {
+
 
             if(!monthViewModel.someSpot.value) ShowNoSpot()
             LazyColumn(
             ) {
                 for (day in daysList) {
-                    item { Row(
-                        modifier = Modifier.padding(vertical = 10.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        var date = LocalDate.of(
-                            monthViewModel.month.value.year,
-                            monthViewModel.month.value.monthNumber,
-                            day.dayInMonth
-                        ).format(
-                            DateTimeFormatter
-                                .ofLocalizedDate(FormatStyle.LONG)
-                        )
-                        Text(
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.Bold,
-                            text = "${day.weekDay}, $date "
+                    item {
+                        Card(
+                            shape = RoundedCornerShape(5.dp),
+                            modifier = Modifier.padding(5.dp),
+                            elevation = 5.dp,
+                            backgroundColor = Color.Transparent
+                        ){
+                            Row(
+                                modifier = Modifier.padding(10.dp),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                var date = LocalDate.of(
+                                    monthViewModel.month.value.year,
+                                    monthViewModel.month.value.monthNumber,
+                                    day.dayInMonth
+                                ).format(
+                                    DateTimeFormatter
+                                        .ofLocalizedDate(FormatStyle.MEDIUM)
+                                )
+                                Text(
+                                    fontSize = 15.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    text = "${day.weekDay}, $date "
 
 
 
 
-                        )
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(10.dp)
-                        ) {
-                            for (spot in spotsList) {
-                                spot.updateInternals()
-                                if(spot.dayInMonth == day.dayInMonth){
-                                    OutlinedButton(
-                                        onClick = { monthViewModel.bookAppointment(spot,day,monthViewModel.month.value.monthNumber)
-                                            navController.popBackStack()}) {
-                                        Text(text = "$spot")
+                                )
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(10.dp)
+                                ) {
+                                    for (spot in spotsList) {
+                                        spot.updateInternals()
+                                        if(spot.dayInMonth == day.dayInMonth){
+                                            OutlinedButton(
+                                                shape = RoundedCornerShape(5.dp),
+                                                onClick = { monthViewModel.bookAppointment(spot,day,monthViewModel.month.value.monthNumber)
+                                                    navController.popBackStack()}) {
+                                                Text(text = "$spot")
+                                            }
+                                        }
+
                                     }
+
                                 }
-
                             }
-
-                        }
-                    } }
+                        } }
 
 
                 }
             }
-        }
+
 
 
     }
