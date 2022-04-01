54c030827f39a2b636f8d9032fa4eb3b04d65584
OlegarioLopez <olelopezlinares@gmail.com>
2022-04-01 19:45:57 +0100

    Update Spot.kt

--- app/src/main/java/com/ole/citastatto/data/Spot.kt
+++ app/src/main/java/com/ole/citastatto/data/Spot.kt
@@ -3,37 +3,39 @@ package com.ole.citastatto.data
 import java.time.Duration
 import java.time.LocalTime
 
-data class Spot (
+data class Spot(
     val month: String = "",
     val dayInMonth: Int = 0,
     var availability: Boolean = true,
     var bookedBy: String = "",
-    var momentIni: List<Int> = listOf(0,0),
-    var momentFin: List<Int> = listOf(0,0),
+    var momentIni: List<Int> = listOf(0, 0),
+    var momentFin: List<Int> = listOf(0, 0),
     var duration: Long = 0,
     var key: Int = 0
 
-){
+) {
     var splitedFrom: Int = 0
+
+    private var momentIniTime: LocalTime? = LocalTime.of(0, 0)
+
+    private var momentFinTime: LocalTime? = LocalTime.of(0, 0)
+
+    init {
+        updateInternals()
+    }
 
     override fun toString(): String {
         updateInternals()
         return "De $momentIniTime a $momentFinTime"
     }
 
-    private var momentIniTime: LocalTime? = LocalTime.of(0,0)
-     private var momentFinTime: LocalTime? = LocalTime.of(0,0)
-
-    init {
-        updateInternals()
+    fun updateInternals() {
+        momentIniTime = LocalTime.of(momentIni[0], momentIni[1])
+        momentFinTime = LocalTime.of(momentFin[0], momentFin[1])
+        duration = Duration.between(momentIniTime, momentFinTime).toMinutes()
+        key = updateKey()
     }
 
-    fun updateInternals(){
-        momentIniTime = LocalTime.of(momentIni[0],momentIni[1])
-        momentFinTime = LocalTime.of(momentFin[0],momentFin[1])
-        duration = Duration.between(momentIniTime,momentFinTime).toMinutes()
-        key=updateKey()
-    }
     private fun updateKey(): Int {
         return "$dayInMonth${momentIni.get(0)}${momentIni.get(1)}".toInt()
     }
